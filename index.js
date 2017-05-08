3fb02dd9e89dffb56de7888b5a070a169d637e3e
aungzanbaw <aungzanbaw@gmail.com>
2017-05-08 22:31:26 +0630

    Promise pattern

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 'use strict'
 let fs = require('fs')
 
-const readFileAsArray = function (file, cb) {
-  fs.readFile(file, function (err, data) {
-    if(err) return cb(err);
-    const lines = data.toString().trim().split("\n")
-    cb(null, lines)
+const readFileAsArray = function (file, cb = () => {}) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(file, function (err, data) {
+      if(err){
+        reject(err)
+        return cb(err);
+      }
+
+      const lines = data.toString().trim().split("\n")
+      resolve(lines)
+      cb(null, lines)
+    })
   })
 }
 
+// Callback pattern still working
 readFileAsArray('./data.txt',function(error, content) {
   console.log(content)
 })
+
+// Promise pattern
+readFileAsArray('./data.txt')
+  .then(lines => {
+    console.log(lines)
+  })
+  .catch(console.error)
